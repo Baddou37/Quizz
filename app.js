ccb34c0a167cc397349cc4161b0e0f89c03ce85e
Julien Thibault <julien.thibault@live.fr>
2022-07-30 13:17:00 +0200

    css

--- app.js
+++ app.js
@@ -1,2 +1,36 @@
 const responses = ["c", "a", "b", "a", "c"];
 const emojis = ["✔️", "✨", "👀", "😭", "👎"];
+
+const form = document.querySelector(".quiz-form")
+form.addEventListener("submit", handleSubmit)
+
+function handleSubmit(e){
+    e.preventDefault()
+
+    const results = [];
+
+    const radioButtons = document.querySelectorAll("input [type='radio']:checked")
+
+    radioButtons.forEach((radioButton, index) => {
+        if(radioButton.value === responses[index]) {
+            results.push(true)
+        } else {
+            results.push(false)
+        }
+    })
+
+    // console.log(radioButtons);
+    // console.log(results);
+    showResults(results);
+}
+
+const titleResult = document.querySelector(".result h2");
+const markResult = document.querySelector(".mark");
+const helpResult = document.querySelector(".help");
+
+function showResults(results){
+
+    const errorsNumber = results.filter(el => el === false).length;
+    console.log(errorsNumber);
+
+}
